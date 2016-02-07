d758edd02613ad0060f7250819fa9c2a5e914c15
John Bywater <john.bywater@appropriatesoftware.net>
2016-02-07 03:16:10 +0000

    Added n-factor markovian price process. Tweaked AWS configuration. Add 'Lift' element. Added converting to datetime from date in various places.

--- quantdsl/domain/model/simulated_price.py
+++ quantdsl/domain/model/simulated_price.py
@@ -2,6 +2,7 @@ import datetime
 import six
 from eventsourcing.domain.model.entity import EventSourcedEntity, EntityRepository
 from eventsourcing.domain.model.events import publish
+from quantdsl.priceprocess.base import datetime_from_date
 
 
 class SimulatedPrice(EventSourcedEntity):
@@ -23,6 +24,7 @@ class SimulatedPrice(EventSourcedEntity):
 
 def register_simulated_price(market_simulation_id, market_name, fixing_date, delivery_date, price_value):
     simulated_price_id = make_simulated_price_id(market_simulation_id, market_name, fixing_date, delivery_date)
+    # raise Exception(simulated_price_id)
     created_event = SimulatedPrice.Created(entity_id=simulated_price_id, value=price_value)
     simulated_price = SimulatedPrice.mutator(event=created_event)
     publish(created_event)
@@ -33,6 +35,8 @@ def make_simulated_price_id(simulation_id, commodity_name, fixing_date, delivery
     assert isinstance(commodity_name, six.string_types), commodity_name
     assert isinstance(fixing_date, (datetime.datetime, datetime.date)), (fixing_date, type(fixing_date))
     assert isinstance(delivery_date, (datetime.datetime, datetime.date)), (delivery_date, type(delivery_date))
+    fixing_date = datetime_from_date(fixing_date)
+    delivery_date = datetime_from_date(delivery_date)
     price_id = ("PriceId(simulation_id='{}' commodity_name='{}' fixing_date='{}', delivery_date='{}')"
                 "".format(simulation_id, commodity_name, fixing_date, delivery_date))
     return price_id
